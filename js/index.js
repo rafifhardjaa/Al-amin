25c16cf460f1a015c980f9597ee768812a9396d5
Rafif Hardja <163794811+rafifhardjaa@users.noreply.github.com>
2024-03-25 08:30:27 +0700

    Add files via upload
    al-amin landing page

---
+++ js/index.js
@@ -0,0 +1,39 @@
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < top + height){
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelectorAll('header nav a [href*=' + id + ']').classList.add('active');
+            })
+        }
+    })
+}
+
+
+// Get the button:
+let myBtnTop = document.getElementById("myBtnTop");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    myBtnTop.style.display = "block";
+  } else {
+    myBtnTop.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
